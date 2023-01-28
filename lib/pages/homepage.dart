1a94649ce34b118a4ecab62eb5b7ac11dfc3175a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2023-01-28 06:47:58 +0200

    Added a statusBuble and a topBar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/utils/statusBubble.dart';
+import 'package:chat_app/utils/topBar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,19 +12,51 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
+              flex: 5,
               child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              color: Colors.indigo,
-              child: SafeArea(
-                child: Column(
-                  children: const [],
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  color: Colors.indigo,
+                  child: SafeArea(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const topBar(),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          'Favorite Contact',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: const [
+                              statusBubble(),
+                              statusBubble(),
+                              statusBubble(),
+                              statusBubble(),
+                              statusBubble(),
+                              statusBubble(),
+                              statusBubble(),
+                              statusBubble(),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          )),
+              )),
           Expanded(
-              flex: 2,
+              flex: 9,
               child: ClipRRect(
                 borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(30),
